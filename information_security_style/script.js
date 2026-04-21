aa678ce8db04465219f9c9d70d341255b31d3756
Silentium_ <misterarsenchink87@gmail.com>
2026-04-21 08:05:50 +0500

    Add files via upload

---
+++ information_security_style/script.js
@@ -0,0 +1,12 @@
+const nav = document.querySelector(".block")
+const burger = document.querySelector(".button")
+
+burger.addEventListener('click', () => {
+    nav.classList.toggle(".active")
+})
+
+
+
+
+    
+
